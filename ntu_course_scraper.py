71f918b42f36fc3f8f83b3a07be7b8fd13e02885
l0rem1psum <sunshine.xuan5960@gmail.com>
2018-05-30 15:56:15 +0800

    Storing course data to database function complete. Add course remark to be added.

--- ntu_course_scraper.py
+++ ntu_course_scraper.py
@@ -3,84 +3,17 @@ from sqlalchemy import create_engine
 import re
 import math
 import numpy as np
+import sqlite3
 from global_config import *
-
-
-def class_schedule_url(course_code):
-    return "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display1?acadsem=" + acad_year + ";" + acad_sem + "&r_search_type=F&r_subj_code=" + course_code + "&boption=Search&staff_access=false"
-
-
-def course_sqlite_database_loc():
-    return "sqlite:///Database/Class_Schedule_" + acad_year + "_" + acad_sem + ".db"
-
-
-def coursenameConverter(coursename):
-    '''Convert all-capitalized course name to course name with only the first letter of each word capitalized.'''
-    converted_coursename = re.sub("[^A-Za-z :,&?()'0-9]", "", coursename)
-    return converted_coursename
-
-
-def acadUnitConverter(acad_unit):
-    return int(acad_unit[0])
-
-
-def course_availiability(coursename):
-    for i in range(-1, -6, -1):
-        if coursename[i].isalnum():
-            if i == -1:
-                return ''
-            return coursename[i + 1:]
-
-
-def indexInfoDF(df1, df2):
-    index_list = df2[0].tolist()
-    index_info_dict = {
-        "course_index": [int(index) for index in index_list if (index == index) and (index != 'INDEX')],
-        "course_code": []
-    }
-    index_info = pd.DataFrame.from_dict(index_info_dict, orient='index')
-    index_info = index_info.replace(np.nan, df1.iat[0, 0])
-    return index_info.transpose()
-
-
-def courseInfoDF(df):
-    course_info = pd.DataFrame({
-        "course_code": [df.iat[0, 0]],
-        "course_name": [coursenameConverter(df.iat[0, 1])],
-        "acad_unit": [acadUnitConverter(df.iat[0, 2])],
-        "availiability": [course_availiability(df.iat[0, 1])],
-    })
-    return course_info
-
-
-# def classInfoDF(df)
-
-
-# df1, df2 = pd.read_html("https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display1?acadsem=2018;1&r_search_type=F&r_subj_code=cz2005&boption=Search&staff_access=false")
-
-# print(df1)
-# print(courseInfoDF(df1))
-# print(indexInfoDF(df1, df2))
-
-
-# courseInfoDF(df1).to_sql("course_info", con=engine, if_exists='append', index=False)
-# indexInfoDF(df1, df2).to_sql("index_info", con=engine, if_exists='append', index=False)
-engine = create_engine(course_sqlite_database_loc(), echo=False)
-l = ['AB0601', 'AB0901', 'AB1000', 'AB1201', 'AB1202', 'AB1301', 'AB1401', 'AB1402', 'AB1501', 'AB1601']
-for mods in l:
-    print("Storing data of course", mods)
-    df1, df2 = pd.read_html("https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display1?acadsem=2018;1&r_search_type=F&r_subj_code=" + mods + "&boption=Search&staff_access=false")
-    courseInfoDF(df1).to_sql("course_info", con=engine, if_exists='append', index=False)
-    indexInfoDF(df1, df2).to_sql("index_info", con=engine, if_exists='append', index=False)
-
-
-####################################################################################
-
-
+# from sqlalchemy import create_engine
 # from sqlalchemy import event, Column, Integer, String, ForeignKey
 # from sqlalchemy.engine import Engine
 # from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import sessionmaker, relationship
+
+
+# def course_sqlite_database_loc():
+#     return "sqlite:///Database/Class_Schedule_" + acad_year + "_" + acad_sem + ".db"
 
 
 # @event.listens_for(Engine, "connect")
@@ -125,22 +58,94 @@ for mods in l:
 # engine = create_engine(course_sqlite_database_loc(), echo=False)
 # Base.metadata.create_all(bind=engine)
 # Session = sessionmaker(bind=engine)
-
 # session = Session()
 
-# course = Course()
-# course.course_code = "CZ1003"
-# course.course_name = "Introduction To Computational Thinking"
-# course.acad_unit = 3
-# course.remark = "*"
+
+def class_schedule_url(course_code):
+    return "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display1?acadsem=" + acad_year + ";" + acad_sem + "&r_search_type=F&r_subj_code=" + course_code + "&boption=Search&staff_access=false"
 
 
-# index = Index()
-# index.course_index = 12345
-# index.course_code = "CZ1004"
+def course_sqlite_database_loc():
+    return "sqlite:///Database/Class_Schedule_" + acad_year + "_" + acad_sem + ".db"
 
-# session.add(course)
-# session.add(index)
 
-# session.commit()
-# session.close()
+def coursenameConverter(coursename):
+    '''Convert all-capitalized course name to course name with only the first letter of each word capitalized.'''
+    converted_coursename = re.sub("[^A-Za-z :,&?()'0-9]", "", coursename)
+    return converted_coursename
+
+
+def acadUnitConverter(acad_unit):
+    return int(acad_unit[0])
+
+
+def course_availiability(coursename):
+    for i in range(-1, -6, -1):
+        if coursename[i].isalnum():
+            if i == -1:
+                return np.nan
+            return coursename[i + 1:]
+
+
+def courseInfoDF(df):
+    course_info = pd.DataFrame({
+        "course_code": [df.iat[0, 0]],
+        "course_name": [coursenameConverter(df.iat[0, 1])],
+        "acad_unit": [acadUnitConverter(df.iat[0, 2])],
+        "availiability": [course_availiability(df.iat[0, 1])],
+    })
+    return course_info
+
+
+def indexInfoDF(df1, df2):
+    index_list = df2[0].tolist()
+    index_info_dict = {
+        "course_index": [int(index) for index in index_list[1:] if index == index],
+        "course_code": []
+    }
+    index_info = pd.DataFrame.from_dict(index_info_dict, orient='index')
+    index_info = index_info.replace(np.nan, df1.iat[0, 0])
+    return index_info.transpose()
+
+
+def classInfoDF(df1, df2):
+    indexes = df2[0].tolist()
+    index_list = []
+    for index in indexes[1:]:
+        if index == index:
+            temp_index = index
+        index_list.append(temp_index)
+
+    start_time = [i[:4] if i == i else np.nan for i in df2[4][1:]]
+    end_time = [i[-4:] if i == i else np.nan for i in df2[4][1:]]
+
+    class_info = {
+        "class_type": df2[1][1:],
+        "class_group": df2[2][1:],
+        "day": df2[3][1:],
+        "start_time": start_time,
+        "end_time": end_time,
+        "venue": df2[5][1:],
+        "course_index": index_list
+    }
+    return pd.DataFrame(class_info)
+
+
+def storeCourseData(coursename):
+    try:
+        engine = create_engine(course_sqlite_database_loc(), echo=False)
+        df1, df2 = pd.read_html(class_schedule_url(coursename))
+        courseInfoDF(df1).to_sql("course_info", con=engine, if_exists='append', index=False)
+        indexInfoDF(df1, df2).to_sql("index_info", con=engine, if_exists='append', index=False)
+        classInfoDF(df1, df2).to_sql("class_info", con=engine, if_exists='append', index=False)
+        return True
+    except:
+        return False
+
+
+for mods in course_list:
+    print("Storing data of course", mods)
+    if storeCourseData(mods):
+        print("Success!")
+        continue
+    print("Failed!")
